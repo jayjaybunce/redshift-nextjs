af7bdaea29d1da31ef12bbb5708ac3f7fb0de80d
Jayjaybunce <jayjaybunce@gmail.com>
2021-07-02 20:46:56 -0600

    adds suport for markdown and components

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    webpack: (config, options) => {
-      config.module.rules.push({
-        test: /\.md/,
-        use: [
-          options.defaultLoaders.babel,
-          {
-            loader: 'raw-loader',
-          },
-        ],
-      })
-  
-      return config
-    },
-  }
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.md/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: 'raw-loader',
+        },
+      ],
+    })
+
+    return config
+  },
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { Flex } from "@chakra-ui/react";
+import React from 'react'
+import { Flex } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <Flex height="20vh" backgroundImage="">
       adasdasdasdadasdasdasdsa
     </Flex>
-  );
+  )
 }
